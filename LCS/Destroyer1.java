cabd77652c774878b9e3f01098f743639b80eb06
NsxT3K0 <161772266+NsxT3K0@users.noreply.github.com>
2024-02-29 19:12:37 -0300

    Add files via upload

---
+++ LCS/Destroyer1.java
@@ -0,0 +1,66 @@
+package LCS;
+import robocode.*;
+//import java.awt.Color;
+
+// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
+
+/**
+ * Destroyer1 - a robot by (your name here)
+ */
+public class Destroyer1 extends Robot
+{
+	/**
+	 * run: Destroyer1's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(100);
+			turnGunRight(180);
+			back(50);
+			turnGunRight(360);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(1);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}
+	others = getOthers();
+
+		// Move to a corner
+		goCorner();
+
+		// Initialize gun turn speed to 3
+		int gunIncrement = 3;
+
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	
+}	
+}
